79d8484cad8bb924896d03d215c5d335e69f4e17
user1 <user1@DESKTOP-MPUKNTA>
2022-12-29 19:38:52 +0900

    ADD : Day02 학생관리 기능 추가
    1. 아이디로 조회 기능 완료
    2. 이름으로 조회 기능 완료(List)
    3. 삭제 기능 완료
    4. 수정 기능 완료

--- src/com/kh/jdbc/day01/student/controller/StudentController.java
+++ src/com/kh/jdbc/day01/student/controller/StudentController.java
@@ -6,15 +6,74 @@ import com.kh.jdbc.day01.student.model.dao.StudentDAO;
 import com.kh.jdbc.day01.student.model.vo.Student;
 
 public class StudentController {
-
+		/**
+		 * 학생 전체 목록 조회
+		 * @param studentName
+		 * @return
+		 */
 		public List<Student> printAll() {
 			StudentDAO sDao = new StudentDAO();
 			List<Student> sList = sDao.selectAll();
 			return sList;
 		}
+		/**
+		 * 학생 이름으로 조회
+		 * @param studentName
+		 * @return
+		 */
+		public List<Student> printAllByName(String studentName) {
+			StudentDAO sDao = new StudentDAO();
+			List<Student> sList = sDao.selectAllByName(studentName);
+			return sList;
+		}
+		/**
+		 * 학생 아이디로 조회
+		 * @param studentId
+		 * @return
+		 */
+		public Student printOneById(String studentId) {
+			StudentDAO sDao = new StudentDAO();
+			Student student = sDao.selectOneById(studentId);
+			return student;
+		}
+		
+		/**
+		 * 회원 등록
+		 * @param student
+		 * @return
+		 */
 		public int registerStudent(Student student) {
 			StudentDAO sDao = new StudentDAO();
 			int result = sDao.insertMemver(student);
 			return result;
 		}
+		/**
+		 * 
+		 * @param studentId
+		 * @return
+		 */
+		public int removeStudent(String studentId) {
+			StudentDAO sDao = new StudentDAO();
+			int result = sDao.deleteMember(studentId);
+			return result;
+			// Void methods cannot return a value
+		}
+		/**
+		 * 
+		 * @param student
+		 * @return
+		 */
+		public int modifyStudent(Student student) {
+			StudentDAO sDao = new StudentDAO();
+			int result = sDao.updateStudent(student);
+			return result;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 }
--- src/com/kh/jdbc/day01/student/model/dao/StudentDAO.java
+++ src/com/kh/jdbc/day01/student/model/dao/StudentDAO.java
@@ -11,19 +11,23 @@ import java.util.List;
 import com.kh.jdbc.day01.student.model.vo.Student;
 
 public class StudentDAO {
-
+	private final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+	private final String USER = "STUDENT";
+	private final String PASSWORD = "STUDENT";
+	private final String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
 	public List<Student> selectAll() {
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "STUDENT";
-		String password = "STUDENT";
+
+		String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+		String USER = "STUDENT";
+		String PASSWORD = "STUDENT";
 		String sql = "SELECT * FROM STUDENT_TBL";
 		Student student = null;
 		List<Student> sList = null;
 		try {
 			// 1. 드라이버 등록
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName(DRIVER_NAME);
 			// 2. DB 연결 생성
-			Connection conn = DriverManager.getConnection(url, user, password);
+			Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
 			// 3. 쿼리문 실행준비(Statement 생성)
 			Statement stmt = conn.createStatement();
 			// 4. 쿼리문 실행 및 결과 받기
@@ -42,6 +46,7 @@ public class StudentDAO {
 				student.setAddress(rset.getString("ADDRESS"));
 				student.setHobby(rset.getString("HOBBY"));
 				student.setEnrollDate(rset.getDate("ENROLL_DATE")); //
+				// 택배 상차
 				sList.add(student);
 				
 				
@@ -64,6 +69,87 @@ public class StudentDAO {
 		return sList;
 	}
 	
+	public List<Student> selectAllByName(String studentName) {
+		List<Student> sList = null;
+		Student student = null;
+		// 쿼리문 작성
+		String sql = "SELECT * FROM STUDENT_TBL WHERE STUDENT_NAME = '"+studentName+"'";
+		try {
+			Class.forName(DRIVER_NAME);
+			Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			Statement stmt = conn.createStatement();
+			ResultSet rset = stmt.executeQuery(sql);
+			sList = new ArrayList<Student>();
+			while(rset.next()) {
+				student = new Student();
+				// setter
+				student.setStudentId(rset.getString("STUDENT_ID"));
+				student.setStudentName(rset.getString("STUDENT_NAME"));
+				student.setStudentPwd(rset.getString("STUDENT_PWD"));
+				student.setGender(rset.getString("GENDER"));
+				student.setAge(rset.getInt("AGE"));
+				student.setEmail(rset.getString("EMAIL"));
+				student.setPhone(rset.getString("PHONE"));
+				student.setAddress(rset.getString("ADDRESS"));
+				student.setHobby(rset.getString("HOBBY"));
+				student.setEnrollDate(rset.getDate("ENROLL_DATE"));
+				// 택배 상차
+				sList.add(student);
+				
+				// 후처리
+			}
+			rset.close();
+			stmt.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return sList;
+	}
+	
+	public Student selectOneById(String studentId) {
+		Student student = null;
+		String sql = "SELECT * FROM STUDENT_TBL WHERE STUDENT_ID = '"+studentId+"'";
+		try {
+			
+			// 1. 드라이버 등록
+			Class.forName(DRIVER_NAME);
+			// 2. DB연결 객체 생성
+			Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			// 3. Statement 생성, 쿼리문 실행준비 완료
+			Statement stmt = conn.createStatement();
+			// 4. 쿼리문 실행 5. 결과 받기
+			ResultSet rset = stmt.executeQuery(sql); //셀렉트 는 쿼리
+			//stmt.executeLargeUpdate(sql);    // DML 은 update를 써서 인트를 받음
+			// 후처리
+			if(rset.next()) {
+				student = new Student();
+				student.setStudentId(rset.getString("STUDENT_ID"));
+				student.setStudentName(rset.getString("STUDENT_NAME"));
+				student.setStudentPwd(rset.getString("STUDENT_PWD"));
+				student.setGender(rset.getString("GENDER"));
+				student.setAge(rset.getInt("AGE"));
+				student.setEmail(rset.getString("EMAIL"));
+				student.setPhone(rset.getString("PHONE"));
+				student.setAddress(rset.getString("ADDRESS"));
+				student.setHobby(rset.getString("HOBBY"));
+				student.setEnrollDate(rset.getDate("ENROLL_DATE"));
+			}
+			// 6. 자원 해제
+			rset.close();
+			stmt.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return student;
+	}
+	
+	
 	public int insertMemver(Student student)	{
 //		// 데이터는 이미 준비 되었음
 //		Student student = new Student();
@@ -75,9 +161,7 @@ public class StudentDAO {
 //		// 데이터 준비 완료
 		
 		// 1. 드라이버 등록
-		String url = "jdbc:oracle:thin:@localhost:1521:XE";
-		String user = "STUDENT";
-		String password = "STUDENT";
+	
 		
 //		String sql = "INSERT INTO STUDENT_TBL VALUES('"+student.getStudentId()+"','"+student.getStudentPwd()+"','"+student.getStudentName()+"','"+student.getGender()+"',"+student.getAge()+",'khuser03@naver.com','01088320393','서울시 동대문구','자전거,축구',SYSDATE)";
 		String sql = "INSERT INTO STUDENT_TBL VALUES("
@@ -93,10 +177,10 @@ public class StudentDAO {
 													+ "SYSDATE)";
 		int result = 0;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection conn = DriverManager.getConnection(url, user, password);
+			Class.forName(DRIVER_NAME);
+			Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
 			Statement stmt = conn.createStatement();
-			// 쿼리문 실행 - DML(INSERT, UPDATE, DELETE
+			// 쿼리문 실행 - DML(INSERT, UPDATE, DELETE)
 			/////////////시험문제/////////////////
 			result = stmt.executeUpdate(sql); // dml의 경우 int 
 			//////////////위에 시험문제 ///////////////////
@@ -108,6 +192,52 @@ public class StudentDAO {
 		}
 		return result;
 	}
+	
+	public int deleteMember(String studentId) {
+		int result = 0;
+		String sql = "DELETE FROM STUDENT_TBL WHERE STUDENT_ID = '"+ studentId +"'";
+		try {
+			Class.forName(DRIVER_NAME);
+			Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			Statement stmt = conn.createStatement();
+			// DML (INSERT, UPDATE, DELETE) -> 중요 시험 : executeUpdate -> int!!!!!!
+			result = stmt.executeUpdate(sql);
+			stmt.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+		// Void methods cannot return a value
+	}
+	
+	public int updateStudent(Student student) {
+		int result = 0;
+		String sql = "UPDATE STUDENT_TBL SET "
+				+ "STUDENT_PWD = '" + student.getStudentPwd() + "', "
+					+ "EMAIL = '" + student.getEmail() + "', "
+						+ "PHONE = '" + student.getPhone() + "', "
+							+ "ADDRESS = '" + student.getAddress() + "', "
+								+ "HOBBY = '" + student.getHobby() +"' "
+									+ "WHERE STUDENT_ID = '" + student.getStudentId() + "'";
+		try {
+			Class.forName(DRIVER_NAME);
+			Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+			Statement stmt = conn.createStatement();
+			result = stmt.executeUpdate(sql);
+			
+			stmt.close();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 	
 	
 	
